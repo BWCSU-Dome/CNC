61b30211dfca922b0ae6b8da3684fa3af73237a1
Jonas <Jonas@Jonas-PC>
2020-06-23 15:25:41 +0200

    Allgemein Klassen hinzugefügt

--- CNC/src/cnc/GUI.java
+++ CNC/src/cnc/GUI.java
@@ -1,5 +1,5 @@
 package cnc;
 
-class Main {
+public class GUI {
 
 }
---
+++ CNC/src/cnc/OutOfAreaException.java
@@ -0,0 +1,12 @@
+package cnc;
+
+public class OutOfAreaException extends Exception {
+
+	   public OutOfAreaException()
+	   {
+		   System.out.println("Die Fr‰se befindet sich auﬂerhalb des zul‰sseigen Bereichs");
+		  
+	   }
+
+
+}
