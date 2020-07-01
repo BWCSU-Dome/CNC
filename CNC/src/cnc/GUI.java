24b70fac9be3785977820414b4fb00949338efb9
DRIEPL <dominikriepl99@outlook.de>
2020-07-01 22:54:42 +0200

    GUI Version 2 - neue Anordnung - erstmalige Funktionalität

--- CNC/src/cnc/GUI.java
+++ CNC/src/cnc/GUI.java
@@ -1,10 +1,9 @@
 package cnc;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.nio.file.Files;
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,23 +11,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
+
 
 public class GUI extends Application{
-	
+
 	private Stage primaryStage;
 	private Font font = new Font("Arial",17);
 	private Font fontBold = new Font("Arial BOLD" ,17);
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
@@ -38,62 +36,86 @@ public class GUI extends Application{
 		
 		//Das ist die WurzelGruppe
 		HBox rootHBox = new HBox();
-
-		//Das ist die Gruppe der InfoAnzeige
-		VBox ctrlVBox = new VBox();
-			ctrlVBox.setPrefSize(350, 1050);
-			ctrlVBox.setSpacing(13);	
-				Label statusInfos = new Label("Statusinfos");
-					statusInfos.setFont(new Font("Arial BOLD",30));
-				ctrlVBox.getChildren().add(statusInfos);
-				
-				Label aktuellePos = new Label("aktuelle Postion: ");
-					aktuellePos.setFont(fontBold);
-				ctrlVBox.getChildren().add(aktuellePos);
-				
-				//Hbox für die Anzeige der X und Y Position
-				HBox posHBox = new HBox();
-					posHBox.setPrefWidth(ctrlVBox.getWidth());
-					posHBox.setSpacing(20);
-					
-					Label aktuellX_static = new Label("X:");
-						aktuellX_static.setFont(font);
-					Label aktuellX = new Label("0000");
-						aktuellX.setFont(font);
-					Label aktuellY_static = new Label("Y:");
-						aktuellY_static.setFont(font);
-					Label aktuellY = new Label("0000");
-						aktuellY.setFont(font);
-				
-					posHBox.getChildren().addAll(aktuellX_static,aktuellX,aktuellY_static,aktuellY);
-					
-				ctrlVBox.getChildren().add(posHBox);
+		
+		//Das ist die gesamte Gruppe der "Menu" Elemente
+		VBox steuerungsVBox = new VBox();
+		
+			//Das ist die Gruppe der Statusinfos
+			HBox statsHBox = new HBox();
+				statsHBox.setPrefSize(550, 300);
+				statsHBox.setAlignment(Pos.TOP_LEFT);
 			
-				//Anzeige des Spindelstatus
-				Label spindelStatus_static = new Label("Spindelstatus:");
-					spindelStatus_static.setFont(fontBold);
-				Label spindelStatus = new Label("aktiv");
-					spindelStatus.setFont(font);
-				ctrlVBox.getChildren().addAll(spindelStatus_static, spindelStatus);
+				//linke Hälfte der stats H-Box
+				VBox statsLeftVBox = new VBox();
+					statsLeftVBox.setSpacing(20);
+					
+					//Das ist die Überschrift
+					Label statusInfos = new Label("Statusinfos");
+						statusInfos.setFont(new Font("Arial BOLD",30));
+						statusInfos.setUnderline(true);
+					statsLeftVBox.getChildren().add(statusInfos);
 				
-				//Anzeige der DrehRichtung
-				Label drehRichtung_static = new Label("DrehRichtung:");
-					drehRichtung_static.setFont(fontBold);
-				Label drehRichtung = new Label("Rechtsdrehung");
-					drehRichtung.setFont(font);
-				ctrlVBox.getChildren().addAll(drehRichtung_static, drehRichtung);
-				
-				//Anzeige Kühlmittelstatus
-				Label kuehlmit_static = new Label("Kühlmittelstatus:");
-					kuehlmit_static.setFont(fontBold);
-				Label kuehlmit= new Label("aktiv");
-					kuehlmit.setFont(font);
-				ctrlVBox.getChildren().addAll(kuehlmit_static, kuehlmit);
-				
-				//Anzeige Geschwindigkeit
-				Label geschwin_static = new Label("Geschwindigkeit:");
-				geschwin_static.setFont(fontBold);
-				
+					
+					Label aktuellePos = new Label("aktuelle Postion: ");
+						aktuellePos.setFont(fontBold);
+					statsLeftVBox.getChildren().add(aktuellePos);
+					
+					//Anzeige des Spindelstatus
+					Label spindelStatus_static = new Label("Spindelstatus:");
+						spindelStatus_static.setFont(fontBold);
+					statsLeftVBox.getChildren().add(spindelStatus_static);
+					
+					//Anzeige der DrehRichtung
+					Label drehRichtung_static = new Label("DrehRichtung:");
+						drehRichtung_static.setFont(fontBold);
+					statsLeftVBox.getChildren().add(drehRichtung_static);
+
+					//Anzeige Kühlmittelstatus
+					Label kuehlmit_static = new Label("Kühlmittelstatus:");
+						kuehlmit_static.setFont(fontBold);
+					statsLeftVBox.getChildren().add(kuehlmit_static);
+
+					//Anzeige Geschwindigkeit
+					Label geschwin_static = new Label("Geschwindigkeit:");
+					geschwin_static.setFont(fontBold);
+					statsLeftVBox.getChildren().add(geschwin_static);
+					
+				statsHBox.getChildren().add(statsLeftVBox);
+					
+				VBox statsRightVBox = new VBox();
+					statsRightVBox.setSpacing(20);
+					//Hbox für die Anzeige der X und Y Position
+					
+					HBox posHBox = new HBox();
+						posHBox.setSpacing(20);
+						
+						Label aktuellX_static = new Label("X:");
+							aktuellX_static.setFont(font);
+						Label aktuellX = new Label("0000");
+							aktuellX.setFont(font);
+						Label aktuellY_static = new Label("Y:");
+							aktuellY_static.setFont(font);
+						Label aktuellY = new Label("0000");
+							aktuellY.setFont(font);
+
+						posHBox.getChildren().addAll(aktuellX_static,aktuellX,aktuellY_static,aktuellY);
+						VBox.setMargin(posHBox, new Insets(58,0,0,0));
+						
+					statsRightVBox.getChildren().add(posHBox);
+						
+						
+					Label spindelStatus = new Label("aktiv");
+						spindelStatus.setFont(font);
+					statsRightVBox.getChildren().add( spindelStatus);
+					
+					Label drehRichtung = new Label("Rechtsdrehung");
+						drehRichtung.setFont(font);
+					statsRightVBox.getChildren().addAll(drehRichtung);
+
+					Label kuehlmit= new Label("aktiv");
+						kuehlmit.setFont(font);
+					statsRightVBox.getChildren().addAll(kuehlmit);
+					
 					//HBox für Geschwindigkeit plus Einheit;
 					HBox geschHBox = new HBox();
 						geschHBox.setSpacing(5);
@@ -102,62 +124,175 @@ public class GUI extends Application{
 						Label geschwinEinheit = new Label("m/min");
 							geschwinEinheit.setFont(font);
 						geschHBox.getChildren().addAll(geschwin, geschwinEinheit);
-					
-				ctrlVBox.getChildren().addAll(geschwin_static, geschHBox);
+						
+					statsRightVBox.getChildren().add(geschHBox);
+						
+				statsHBox.getChildren().add(statsRightVBox);
 				
-				//Anzeige der Console
-					TextArea console = new TextArea("Hallooooooooooooooooo\noooooooooooooooooo\nooooooooooooooooooo\noooooooooooooooooo\noooooooooooooooo\noooooooo\nooooooo");
-					console.setPrefSize(ctrlVBox.getPrefWidth(), ctrlVBox.getPrefHeight());
-					console.setFont(new Font("Arial", 17));
-					console.setEditable(false);
-
-				ctrlVBox.getChildren().add(console);
+			steuerungsVBox.getChildren().add(statsHBox);
+			
+			//Das ist die Gruppe der Buttons und Console
+			HBox ctrlBtnHBox = new HBox();
+				ctrlBtnHBox.setPrefSize(550, 750);
 				
-				//Eingabefeld
-				
-				TextField eingabe_TF = new TextField("Geben Sie hier Ihre Codes ein");
-					eingabe_TF.setPrefWidth(ctrlVBox.getPrefWidth());
-					eingabe_TF.setFont(new Font("Arial", 17));
-				ctrlVBox.getChildren().add(eingabe_TF);
-				VBox.setMargin(eingabe_TF, new Insets(10,2,15,2));
-				
-		rootHBox.getChildren().add(ctrlVBox);
+				//Das ist die Gruppe der Console
+				VBox ctrlVBox = new VBox();
+					ctrlVBox.setPrefSize(350, ctrlBtnHBox.getPrefHeight());
+					ctrlVBox.setSpacing(13);	
+						
+						//Anzeige der Console
+							TextArea console = new TextArea("N10 M03\r\n" + 
+															"N20 M08\r\n" + 
+															"N30 G01 X0 Y0\r\n" + 
+															"N40 G02 X0 Y10 I0 J5\r\n" + 
+															"N50 G01 X10 Y10\r\n" + 
+															"N60 G02 X10 Y0 I0 J-5\r\n" + 
+															"N70 G28\r\n" + 
+															"N80 M00");
+							console.setPrefSize(ctrlVBox.getPrefWidth(), ctrlVBox.getPrefHeight());
+							console.setFont(new Font("Arial", 17));
+							console.setEditable(false);
 		
-		//Das ist die Gruppe der Buttons
-		VBox btnVBox = new VBox();
-			btnVBox.setPrefSize(200, 1050);
-			btnVBox.setSpacing(30);
-			btnVBox.setAlignment(Pos.TOP_CENTER);
+						ctrlVBox.getChildren().add(console);
+						
+						//Eingabefeld
+						
+						TextField eingabe_TF = new TextField("Geben Sie hier Ihre Codes ein");
+							eingabe_TF.setPrefWidth(ctrlVBox.getPrefWidth());
+							
+							//Diese Methode dient dazu, dass das EingabeFeld beim ersten aktivieren geleert wird. 
+							eingabe_TF.setOnMousePressed((EventHandler<Event>) new EventHandler<>() {
+								@Override
+								public void handle(Event arg0) {
+									if(eingabe_TF.getText().equals("Geben Sie hier Ihre Codes ein") ) {
+										eingabe_TF.setText("");
+									}
+								}
+							});
+							eingabe_TF.setFont(new Font("Arial", 17));
+		
+						ctrlVBox.getChildren().add(eingabe_TF);
+						VBox.setMargin(eingabe_TF, new Insets(10,2,15,2));
+						
+				ctrlBtnHBox.getChildren().add(ctrlVBox);
 			
-			int btn_width = 150;
-			int btn_height = 50;
+				//Das ist die Gruppe der Buttons
+				VBox btnVBox = new VBox();
+					btnVBox.setPrefSize(200, ctrlBtnHBox.getPrefWidth());
+					btnVBox.setSpacing(30);
+					btnVBox.setAlignment(Pos.CENTER);
+					
+					int btn_width = 170;
+					int btn_height = Integer.MAX_VALUE;
+					
+						Button startBtn = new Button("Start");
+							startBtn.setPrefSize(btn_width, btn_height);
+							startBtn.setFont(fontBold);
+							
+							//Dieser Button startet/pausiert die bereits eingebenen Codes
+							startBtn.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+								
+								@Override
+								public void handle(ActionEvent arg0) {
+									switch(startBtn.getText()) {
+									case "Start":
+										startBtn.setText("Pause");
+										break;
+									case "Pause":
+										startBtn.setText("Start");
+										break;
+									}						
+								}
+							});
+							
+						btnVBox.getChildren().add(startBtn);
+
+						Button notStopBtn = new Button("Not - Stop");
+							notStopBtn.setStyle("-fx-background-color: black; -fx-text-fill: red");
+							notStopBtn.setFont(new Font("Arial BOLD" ,22));
+							notStopBtn.setPrefSize(btn_width, btn_height);
+							
+							//Diese Aktionen dienen dazu, dass der Button seine Farbe beim Betreten/Verlassen der Maus ändert
+							notStopBtn.setOnMouseEntered((EventHandler<Event>) new EventHandler<>() {
+								
+								@Override
+								public void handle(Event arg0) {
+									notStopBtn.setStyle("-fx-background-color: red; -fx-text-fill: black");
+								}
+							});
+							notStopBtn.setOnMouseExited((EventHandler<Event>) new EventHandler<>() {
+								@Override
+								public void handle(Event arg0) {
+									notStopBtn.setStyle("-fx-background-color: black; -fx-text-fill: red");
+								}
+							});
+							
+						btnVBox.getChildren().add(notStopBtn);
+						
+						Button kuehlmitBtn = new Button("Kühlmittel\naktivieren");
+							kuehlmitBtn.setPrefSize(btn_width, btn_height);
+							kuehlmitBtn.setFont(fontBold);
+							
+							//Das dient dazu, dass das Kühlmittel de/aktivert werden kann
+							kuehlmitBtn.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+
+								@Override
+								public void handle(ActionEvent arg0) {
+									switch(kuehlmitBtn.getText()) {
+									case "Kühlmittel\naktivieren":
+										kuehlmit.setText("aktiviert");
+										kuehlmitBtn.setText("Kühlmittel\ndeaktivieren");
+										// Hier muss noch der Boolean der Hauptklasse geändert werden.
+										break;
+									case "Kühlmittel\ndeaktivieren":
+										kuehlmit.setText(" deaktiviert");
+										kuehlmitBtn.setText("Kühlmittel\naktivieren");
+										//Hier muss noch der Boolean der Hauptklasse geändert werden.
+									}
+								}
+							});
+							
+						btnVBox.getChildren().add(kuehlmitBtn);
+						
+						
+						
+						// Dieser Button dient dazu, dass der eingegebne String in einen Code zerlegt wird.
+						Button addCodeBtn = new Button("Code\nhinzufügen");
+							addCodeBtn.setPrefSize(btn_width, btn_height);
+							addCodeBtn.setFont(fontBold);
+							
+							addCodeBtn.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+								@Override
+								public void handle(ActionEvent arg0) {
+									System.out.println(eingabe_TF.getText());
+									eingabe_TF.setText("");	
+								}
+							});
+							
+							
+						btnVBox.getChildren().add(addCodeBtn);
+						
+						Button clearBtn = new Button("Anwendung\nbeenden"); /// Im Moment Fenter schließen
+							clearBtn.setPrefSize(btn_width, btn_height);
+							clearBtn.setFont(fontBold);
+							
+							clearBtn.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	
+								@Override
+								public void handle(ActionEvent arg0) {
+									primaryStage.close();
+								}
+							});
+							
+						btnVBox.getChildren().add(clearBtn);
+						VBox.setMargin(clearBtn, new Insets(0,0,15,0));
+						
+						
+				ctrlBtnHBox.getChildren().add(btnVBox);
 			
-				Button startBtn = new Button("Start");
-					startBtn.setPrefSize(btn_width, btn_height);
-				btnVBox.getChildren().add(startBtn);
-				
-				FileInputStream fip = new FileInputStream("/C:/Users/driepl/git/CNC/CNC/files/Notstop.png");	//Wäre Nice2have, aber Problem mit Dateipfad
-				Image notStopImage = new Image(fip);
-				ImageView notStopView = new ImageView(notStopImage);
-				
-				Button notStopBtn = new Button("",notStopView );
-					notStopBtn.setPrefSize(btn_width, btn_height);
-				btnVBox.getChildren().add(notStopBtn);
-				
-
-				Button kuehlmitBtn = new Button("Kühlmittel aktiv");
-					kuehlmitBtn.setPrefSize(btn_width, btn_height);
-				btnVBox.getChildren().add(kuehlmitBtn);
-				
-				Button addCodeBtn = new Button("Code hinzufügen");
-					addCodeBtn.setPrefSize(btn_width, btn_height);
-					btnVBox.getChildren().add(addCodeBtn);
-				
-				Button clearBtn = new Button("Clear Input");
-					clearBtn.setPrefSize(btn_width, btn_height);
-				btnVBox.getChildren().add(clearBtn);
-
-		rootHBox.getChildren().add(btnVBox);
+			steuerungsVBox.getChildren().add(ctrlBtnHBox);
+			
+		rootHBox.getChildren().add(steuerungsVBox);
 		
 		//Das ist die Arbeitsfläche
 		Pane arbeitsF = new Pane();
