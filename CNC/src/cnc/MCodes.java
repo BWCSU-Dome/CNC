c240a7442823d7edb47ef958d2e1e0af056f598e
Jonas <Jonas@Jonas-PC>
2020-07-02 19:23:04 +0200

    Methoden haben Funktionen erhalten: ändern Variablenwerte in Hauptklasse

--- CNC/src/cnc/MCodes.java
+++ CNC/src/cnc/MCodes.java
@@ -5,7 +5,7 @@ public class MCodes extends Codes {
 	
 	//Repräsentation von Code M00
 	public static void haltMaschine() {
-		
+		Main.setAktGeschw(0);
 	}
 	
 	//Repräsentation von Code M02
@@ -15,17 +15,18 @@ public class MCodes extends Codes {
 	
 	//Repräsentation von Code M03 und M04 --> Mitgegeben wird die Richtung (Linksdrehung: 0, Rechtsdrehung: 1)
 	public static void schalteSpindelAn(boolean richtung) {
-		
+		Main.setSpindelAktiv(true);
+		Main.setSpindelRechtslaufAktiv(richtung);
 	}
 	
 	//Repräsentation von Code M05
 	public static void schalteSpindelAus() {
-		
+		Main.setSpindelAktiv(false);
 	}
 	
 	//Repräsentation von Code M08/M09 --> Mitgegeben wird gewünschter Status (An/Aus)
 	public static void schalteKuelmittel(boolean onoff) {
-		
+		Main.setKuehlungAktiv(onoff);
 	}
 	
 }
