1a9bad71b609e820ad54932774a6319375c83f80
Jonas <Jonas@Jonas-PC>
2020-07-06 11:51:06 +0200

    Fehlgeschlagene Überprüfungen werfen nun IOException

--- CNC/src/cnc/RegularExpression.java
+++ CNC/src/cnc/RegularExpression.java
@@ -1,10 +1,11 @@
 package cnc;
 import java.util.regex.Pattern;
+import java.io.IOException;
 import java.lang.String;
 
 public class RegularExpression {	
-		
-		public static void checkCodeFormatierung(String code) {
+	
+	public static void checkCodeFormatierung(String code) {
 			//Unser Einganstext, der den Code der auzuführenden Funktion (ggf mit Argumenten) enthält
 			
 			
@@ -12,16 +13,22 @@ public class RegularExpression {
 			//Krit1: richtiger G oder M Code
 			//Krit2: valide Argumente vorhanden: Kein Unsinn, nur erlaubte Buchstaben (als Parameter)
 			//Wenn nicht, wird die Methode hier abgebrochen
-			if(!Pattern.matches("((M00|m00|M02|m02|M03|m03|M04|m04|M05|m05|M08|m08|M13|m13|M14|m14)|(G00|g00|G01|g01|G02|g02|G03|g03|G28|g28))\\s*[XxYyJjIi0-9 ]*$",code)) {
-				System.out.println("Befehl nicht vorhanden, oder Syntax falsch.");
-				return;
-			}
 			
-								
-			if(code.split(" ").length > 5) {		//Check auf maximal mögliche Anzahl der Argumente. Wenn zu viele Argumente mitgegeben wurden, wird die Methode hier abgebrochen
-				System.out.println("Es wurden zu viele Argumente eingegeben.");
+			try {
+				
+			if(!Pattern.matches("((M00|m00|M02|m02|M03|m03|M04|m04|M05|m05|M08|m08|M13|m13|M14|m14)|(G00|g00|G01|g01|G02|g02|G03|g03|G28|g28))\\s*[XxYyJjIi0-9 ]*$",code)) {
+				throw new IOException("Befehl nicht vorhanden, oder Syntax falsch.");
+			}
+				if(code.split(" ").length > 5) {		//Check auf maximal mögliche Anzahl der Argumente. Wenn zu viele Argumente mitgegeben wurden, wird die Methode hier abgebrochen
+					throw new IOException("Es wurden zu viele Argumente eingegeben.");
+				}
+			
+			
+			} catch(IOException e) {
+				System.out.println(e.getMessage());
 				return;
 			}
+								
 			//Aufruf zum Hinzufügen zur Befehlsschlange
 			Codes.enqueueBefehl(code);
 				return;
