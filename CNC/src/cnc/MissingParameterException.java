2e56ac69e39851b3cc0f8694f70ca3656bed0d2c
derba <derba@DESKTOP-09HT8J1.fritz.box>
2020-08-25 07:47:16 +0200

    SerialVersionUID hinzugefügt (wollte Eclipse unbedingt)

--- CNC/src/cnc/MissingParameterException.java
+++ CNC/src/cnc/MissingParameterException.java
@@ -2,6 +2,11 @@ package cnc;
 
 public class MissingParameterException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public MissingParameterException() {
 		Codes.addStringToOutput("*Fehlende Parameter für Code*");
 	}
--- CNC/src/cnc/OutOfAreaException.java
+++ CNC/src/cnc/OutOfAreaException.java
@@ -2,7 +2,12 @@ package cnc;
 
 public class OutOfAreaException extends Exception {
 
-	   public OutOfAreaException()
+	   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OutOfAreaException()
 	   {
 		   super("Die Fräse befände sich während der Ausführung des Befehls außerhalb des zulässigen Bereichs");
 		   Codes.addStringToOutput("*Out Of Area für Code*");
